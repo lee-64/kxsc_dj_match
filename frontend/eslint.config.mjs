72517084942cc5749179297fc608fe7a2adc811a
lee <leestilwell05@gmail.com>
2025-01-05 13:26:32 -0800

    ESLint configuration

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: (await import('@babel/eslint-parser')).default,
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  }
+];
 
 export default eslintConfig;
